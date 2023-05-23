c95a094ff8758e34705d3fc798b57155c070a01c
nutzallstar <nutzishot_6@hotmail.com>
2023-05-23 11:42:56 +0700

    New Unit test

---
+++ hello/src/test/java/com/example/hello/HelloControllerTest.java
@@ -0,0 +1,21 @@
+package com.example.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void sayHi() {
+        HelloResponse body = testRestTemplate.getForObject("/hello/demo", HelloResponse.class);
+        assertEquals("Hello ... demo", body.getMessage());
+    }
+
+}
